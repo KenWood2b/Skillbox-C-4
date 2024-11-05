abd32fcb41d2d31aa8514ed7e0b43998ee8c766a
KenWood2b <kenwood2b@gmail.com>
2024-11-05 22:09:30 +0300

    First

---
+++ MethodsConsoleApp/Exercise2ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрашиваем ввод строки от пользователя
+            Console.WriteLine("Введите длинное предложение:");
+            string input = Console.ReadLine();
+
+            // Обратная перестановка слов и вывод результата
+            string reversedSentence = ReverseWords(input);
+            Console.WriteLine("\nПредложение с обратным порядком слов:");
+
+            // Разделение перевернутого предложения на слова и вывод каждого слова на новой строке
+            string[] words = SplitText(reversedSentence);
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadKey();
+        }
+
+        // Метод для разделения текста на слова
+        static string[] SplitText(string text)
+        {
+            return text.Split(' '); // Разделение строки по пробелу
+        }
+
+        // Метод для перестановки слов в обратном порядке
+        static string ReverseWords(string inputPhrase)
+        {
+            // Разделяем предложение на слова
+            string[] words = SplitText(inputPhrase);
+
+            // Объединяем слова в обратном порядке
+            string reversedSentence = "";
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                reversedSentence += words[i];
+                if (i > 0) reversedSentence += " "; // Добавляем пробел, кроме последнего слова
+            }
+
+            return reversedSentence;
+        }
+    }
+}
---
+++ MethodsConsoleApp/MethodsConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Запрашиваем ввод строки от пользователя
+        Console.WriteLine("Введите длинное предложение:");
+        string input = Console.ReadLine();
+
+        // Разделяем текст на слова и выводим их
+        string[] words = SplitText(input);
+        PrintWords(words);
+    }
+
+    // Метод для разделения текста на слова
+    static string[] SplitText(string text)
+    {
+        return text.Split(' '); // Разделение строки по пробелу
+    }
+
+    // Метод для вывода каждого слова на новой строке
+    static void PrintWords(string[] words)
+    {
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+        Console.ReadKey();
+    }
+}
